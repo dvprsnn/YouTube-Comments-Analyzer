da48be7849b2fc5588c823caca86a272a1b98c8d
Prasanna devi <128495906+dvprsnn@users.noreply.github.com>
2024-12-01 18:12:30 +0530

    Delete Vid_Comment directory

--- Vid_Comment/Video_classifier.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy
-from sklearn.linear_model import  LinearRegression, LogisticRegression
-
-def content_classifier(datas):
-    df = pd.DataFrame(datas)
-    print(df)
